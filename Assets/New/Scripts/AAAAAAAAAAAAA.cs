3978565f1c295c6b4ed6b366d6b3a97a8a8274ce
Hayden Thorpe <87242997+ThorpeDevs@users.noreply.github.com>
2026-02-03 16:57:01 +0000

    Apply force to rock Rigidbody; prefab updates
    Add a Rigidbody2D reference and force application to the rock script, initializing a random Force in Start() and applying it each Update via AddRelativeForce. Update BigBadBrick prefab to reference RockRigidBody. Modify PlayerTreangle prefab: replace a CompositeCollider2D component with a PlayerInput MonoBehaviour and a PolygonCollider2D, mark collider as trigger and set polygon points/tiling properties. Update scene file to remove/replace some prefab instances and adjust one prefab instance's local position values accordingly.

--- Assets/New/Scripts/AAAAAAAAAAAAA.cs
+++ Assets/New/Scripts/AAAAAAAAAAAAA.cs
@@ -3,7 +3,19 @@ using UnityEngine.SceneManagement;
 
 public class AAAAAAAAAAAAA : MonoBehaviour
 {
+    public Rigidbody2D RockRigidBody;
     private Scene CurrentScene; // Holds The Current Scene
+    private Vector2 Force;
+
+    private void Start() // NEED TO USE THIS https://www.youtube.com/watch?v=wzQ9Xn406wc
+    {
+        Force = new Vector2(Random.Range(-20, 20), Random.Range(-20, 20));
+    }
+
+    private void Update()
+    {
+        RockRigidBody.AddRelativeForce(Force * 5 * Time.deltaTime);
+    }
 
     private void OnTriggerEnter2D(Collider2D other)
     {
